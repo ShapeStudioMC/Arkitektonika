75660207b8b51819cb40aa754343f6881247353a
ffd000 <hartleyterw@gmail.com>
2026-02-09 12:14:18 +0200

    add config

---
+++ config.js
@@ -0,0 +1,12 @@
+{
+  "port": 3000,
+  "prune": 1800000,
+  "maxIterations": 20,
+  "maxSchematicSize": 1000000,
+  "allowedOrigin": "*",
+  "limiter": {
+    "windowMs": 60000,
+    "delayAfter": 30,
+    "delayMs": 500
+  }
+}
